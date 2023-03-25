062a7c89db75ac0a6ce2f0f478a386789a86962b
Saptarshi Dey <saptarshid795.tal@gmail.com>
2023-03-25 18:30:41 +0530

    Reformatted old files

--- js-css/Calculate.js
+++ js-css/Calculate.js
@@ -1,73 +1,73 @@
-function HCF(x,y){
-    while(x!=y)
-    {
-      if(x>y)
-      x=x-y;
-      else
-      y=y-x;
-    }
-    return x;
+function HCF(x, y) {
+  while (x != y) {
+    if (x > y)
+      x = x - y;
+    else
+      y = y - x;
+  }
+  return x;
 }
-function getHCF(){
-   var a= parseInt(document.getElementById("num1").value);
-   var b= parseInt(document.getElementById("num2").value);
-   var res=HCF(a,b);
-   alert("The HCF of the given numbers is = "+res);
-}
-
-function getLCM(){
-    var a= parseInt(document.getElementById("num1").value);
-    var b= parseInt(document.getElementById("num2").value);
-    var res=HCF(a,b);
-    res=(a*b)/res;
-    alert("The LCM of the given numbers is = "+res);
- }
-
-var π=Math.PI;
-
-function log(a){
-  var n=Math.log(10);a
-  return Math.log(a)/n;
-}
-function ln(a){ return Math.log(a); }
-function exp(a){ return Math.exp(a); }
-
-function toDegree(x){
-  return (x*180)/Math.PI;
+function getHCF() {
+  var a = parseInt(document.getElementById("num1").value);
+  var b = parseInt(document.getElementById("num2").value);
+  var res = HCF(a, b);
+  alert("The HCF of the given numbers is = " + res);
 }
 
-function CLS(){
-   document.getElementById("screen").innerText="";
-   document.getElementById("result").innerText="0.000";
+function getLCM() {
+  var a = parseInt(document.getElementById("num1").value);
+  var b = parseInt(document.getElementById("num2").value);
+  var res = HCF(a, b);
+  res = (a * b) / res;
+  alert("The LCM of the given numbers is = " + res);
 }
 
-function inputHelper(x){ document.getElementById("screen").innerHTML += x; }
+var π = Math.PI;
 
-function DEL(){
-    var input=document.getElementById("screen").innerText;
-    var ch=input.charAt(input.length-2);
-    if(ch=='h') input=input.substring(0,input.length-5);
-    else if((ch=='n' && input.charAt(input.length-3)!='l') || ch=='s' || ch=='g' || ch=='p' || ch=='w') input=input.substring(0,input.length-4);
-    else if(ch=='n' && input.charAt(input.length-3)=='l') input=input.substring(0,input.length-3);
-    else input=input.substring(0,input.length-1);
-    document.getElementById("screen").innerText=input;
+function log(a) {
+  var n = Math.log(10); a
+  return Math.log(a) / n;
+}
+function ln(a) { return Math.log(a); }
+function exp(a) { return Math.exp(a); }
+
+function toDegree(x) {
+  return (x * 180) / Math.PI;
 }
 
-function Calculate(){
+function CLS() {
+  document.getElementById("screen").innerText = "";
+  document.getElementById("result").innerText = "0.000";
+}
+
+function inputHelper(x) { document.getElementById("screen").innerHTML += x; }
+
+function DEL() {
+  var input = document.getElementById("screen").innerText;
+  var n = 0, ch = input.charAt(input.length - 2);
+  if (ch == 'h') n = 5;
+  else if (ch == '0' && input.charAt(input.length - 1) == '(') n = 6;
+  else if ((ch == 'n' && input.charAt(input.length - 3) != 'l') || ch == 's' || ch == 'g' || ch == 'p' || ch == 'w') n = 4;
+  else if (ch == 'n' && input.charAt(input.length - 3) == 'l') n = 3;
+  else n = 1;
+  document.getElementById("screen").innerText = input.substring(0, input.length - n);
+}
+
+function Calculate() {
   try {
-    var input=document.getElementById("screen").innerText;
-    input = input.replaceAll("×","*");
-    input = input.replaceAll("÷","/"); 
-    input = input.replaceAll("π","PI");
-    var value=math.evaluate(input);
-    document.getElementById("result").innerText=value.toFixed(3);
+    var input = document.getElementById("screen").innerText;
+    input = input.replaceAll("×", "*");
+    input = input.replaceAll("÷", "/");
+    input = input.replaceAll("π", "PI");
+    var value = math.evaluate(input);
+    document.getElementById("result").innerText = value.toFixed(3);
   } catch (err) {
     console.log(input);
-    document.getElementById("result").innerText="Malformed Expression";
+    document.getElementById("result").innerText = "Malformed Expression";
   }
 }
 
-document.addEventListener("contextmenu",function(arg){
+document.addEventListener("contextmenu", function (arg) {
   arg.preventDefault();
 });
 
